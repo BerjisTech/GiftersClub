60305adf5a155c86b98bedd9497b87513557f7d3
= <=>
2025-06-29 20:19:16 +0300

    Fix Query param name quoting: use distinct and quoted 'query' filter

--- app/src/main/java/club/gifters/giftersclub/network/SearchQueriesApi.kt
+++ app/src/main/java/club/gifters/giftersclub/network/SearchQueriesApi.kt
@@ -13,8 +13,9 @@ interface SearchQueriesApi {
      */
     @GET("search_queries")
     suspend fun searchQueries(
-        @Query("select", encoded = true) select: String = "distinct(query)",
-        @Query("query", encoded = true) queryFilter: String,
+        @Query("select",   encoded = true) select: String   = "query",
+        @Query("distinct", encoded = true) distinct: String = "query",
+        @Query("\"query\"", encoded = true) queryFilter: String,
         @Query("order", encoded = true) order: String = "suggestion_index.desc,result_clicked_index.desc",
         @Query("limit") limit: Int = 10
     ): List<SearchQuery>
