93f70057cf192ec7b3fe27b39ea5ec4709d73eff
= <=>
2025-07-02 15:48:14 +0300

    Update gradient angles from 45 to 90 degrees across multiple drawable resources for improved visual consistency. Adjusted background in item_post layout and added new string resource for lock icon representation.

--- scripts/generate_gradients.py
+++ scripts/generate_gradients.py
@@ -61,7 +61,7 @@ def main():
 
     xml_template = '''<shape xmlns:android="http://schemas.android.com/apk/res/android">
     <gradient
-        android:angle="45"
+        android:angle="90"
         android:startColor="@color/{start}_SHADE"
         android:endColor="@color/{end}_SHADE"
         android:type="linear" />
