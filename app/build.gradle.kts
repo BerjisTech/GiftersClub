ec9974c9d6495bf73d3081b1f359e31ac9daa90f
= <=>
2025-07-30 10:57:52 +0300

    fix(deployment): update device identifier and timestamp in deployment target selector chore: increment version code and name to 1.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "club.gifters.giftersclub"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.0.11"
+        versionCode = 13
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
